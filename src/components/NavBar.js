331b7763ba04de71876a4708857c2ceb0057a8b8
Calvin Liu <calvin.liu68@gmail.com>
2021-05-06 21:49:02 -0600

    cleanup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
 import { Link } from 'react-scroll'
 import { FaBars, FaHome, FaTimes } from 'react-icons/fa';
 import Button from './Button';
