db6b2fd9b191bb4ac048c55caeb2cfcef8580557
Calvin Liu <calvin.liu68@gmail.com>
2021-05-18 09:54:51 -0600

    update projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
             <ProjectItem
               path='https://github.com/liucalvin/8-Puzzle'
               text='8 Puzzle'
-              desc='The 8 Puzzle game on android, with an interactive UI and puzzle solver. 
+              desc='The 8 Puzzle game on Android, with an interactive UI and puzzle solver. 
                 The solver uses the A* algorithm to find the optimal solution to the puzzle.'
               src='/images/8-puzzle-demo.png'
               alt='8 Puzzle Image'
@@ -26,9 +26,28 @@ function Projects() {
               alt='Seam Carver Image'
               label='java' />
             <ProjectItem
+              path='https://github.com/liucalvin/memorybee'
+              text='Memorybee'
+              desc='A flashcard site built with React that auto generates cards using word definitions and
+                 usages from WordsAPI, with a backend rest api using PostgreSQL and JWT authentication.'
+              src='/images/memorybee-image.png'
+              alt='Memorybee Image'
+              label='javascript' />
+          </ul>
+          <ul className='project-items'>
+            <ProjectItem
+              path='https://github.com/liucalvin/website'
+              text='Personal Website'
+              desc='Developed a fully responsive website with custom React components,
+              bootstrapped with Create React App.'
+              src='/images/website-image.png'
+              alt='Website Image'
+              label='javascript' />
+            <ProjectItem
               path='https://github.com/liucalvin/reflect-api'
               text='Reflect'
               desc='A mood tracking and journaling app on android, built with a backend rest api
+                with Express and MongoDB
                 for user registration, login, storage, and authentication.'
               src='/images/under-construction.png'
               alt='Reflect Image'
